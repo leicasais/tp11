8e243aa3c1d073a538a1eaeb0de7aa1910399f7d
leicasais <lcasais@itba.edu.ar>
2025-06-04 16:11:49 -0300

    export_pin intento 0

--- hardware.c
+++ hardware.c
@@ -1,13 +1,18 @@
 #include "hardware.h"
 
 
-void export_pin (int num_pin){
+void export_pin (const int num_pin){
     FILE *doc;
-    int error;
+    int error_w;
+    char pin_str[5];  // Suficiente para almacenar "9999" como máximo
+
+     // Convertir el número a cadena
+    snprintf(pin_str, sizeof(pin_str), "%d", num_pin);
 
     if((doc = fopen("/sys/class/gpio/export","w")) == NULL){
         printf("No se pudo exportar el pin %d. Intente mas tarde\n", num_pin);
-        exit();
+        exit(-1);//error de export
     }
+    error_w=fputs(, );
 
 }
