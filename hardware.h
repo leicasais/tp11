0d2af9663bbd4e51d1978d8c599d7fec048c0085
leicasais <lcasais@itba.edu.ar>
2025-06-04 14:33:41 -0300

    ejercicio 3, mapeo LED fisico (GPIO)

---
+++ hardware.h
@@ -0,0 +1,14 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+//Mapeo LED fisico (GPIO)
+#define LED_0 17
+#define LED_1 4
+#define LED_2 18
+#define LED_3 23
+#define LED_4 24
+#define LED_5 25
+#define LED_6 22
+#define LED_7 27
+
+#endif
